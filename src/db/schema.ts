f5d799b219a8373f2fa6b2d5f349cbdf6b447d17
Dawidu7 <dawidtomalik7@gmail.com>
2023-10-05 10:04:19 +0200

    Added NOT NULL Constraints

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,26 +2,30 @@ import { bigint, boolean, date, pgTable, varchar } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
   id: varchar("id", { length: 15 }).primaryKey(),
-  email: varchar("email", { length: 255 }).unique(),
-  handle: varchar("handle", { length: 20 }).unique(),
-  name: varchar("name", { length: 20 }),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  handle: varchar("handle", { length: 20 }).notNull().unique(),
+  name: varchar("name", { length: 20 }).notNull(),
   createdAt: date("created_at").defaultNow(),
 })
 
 export const keys = pgTable("keys", {
   id: varchar("id", { length: 15 }).primaryKey(),
-  userId: varchar("user_id", { length: 15 }).references(() => users.id, {
-    onDelete: "cascade",
-  }),
+  userId: varchar("user_id", { length: 15 })
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
   hashedPassword: varchar("hashed_password", { length: 255 }),
-  primary: boolean("primary"),
+  primary: boolean("primary").notNull(),
 })
 
 export const sessions = pgTable("sessions", {
   id: varchar("id", { length: 15 }).primaryKey(),
-  userId: varchar("user_id", { length: 15 }).references(() => users.id, {
-    onDelete: "cascade",
-  }),
-  activeExpires: bigint("active_expires", { mode: "bigint" }),
-  idleExpires: bigint("idle_expires", { mode: "bigint" }),
+  userId: varchar("user_id", { length: 15 })
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+  activeExpires: bigint("active_expires", { mode: "bigint" }).notNull(),
+  idleExpires: bigint("idle_expires", { mode: "bigint" }).notNull(),
 })
